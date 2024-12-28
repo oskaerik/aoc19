6407b945449328b4e92208c268b714000cad80a5
Oskar Eriksson <oskaerik@gmail.com>
2024-12-28 15:08:09 +0100

    Add 01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,55 @@
+use std::cmp;
+use std::fs;
+
 fn main() {
-    println!("Hello, world! 01");
+    let input = fs::read_to_string("input/01.in").unwrap();
+    let masses: Vec<i64> = input.lines().map(|line| line.parse().unwrap()).collect();
+
+    let p1 = solve_p1(&masses);
+    println!("Part One: {p1}");
+
+    let p2 = solve_p2(&masses);
+    println!("Part Two: {p2}");
+}
+
+fn solve_p1(masses: &[i64]) -> i64 {
+    masses.iter().map(|&mass| fuel(mass)).sum()
+}
+
+fn solve_p2(masses: &[i64]) -> i64 {
+    masses.iter().map(|&mass| fuel_recursive(mass)).sum()
+}
+
+fn fuel(mass: i64) -> i64 {
+    cmp::max(mass / 3 - 2, 0)
+}
+
+fn fuel_recursive(mass: i64) -> i64 {
+    let mut x = fuel(mass);
+    if x > 0 {
+        x += fuel_recursive(x);
+    }
+    x
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fuel() {
+        assert_eq!(fuel(12), 2);
+        assert_eq!(fuel(14), 2);
+        assert_eq!(fuel(1969), 654);
+        assert_eq!(fuel(100756), 33583);
+        assert_eq!(fuel(-1), 0);
+    }
+
+    #[test]
+    fn test_fuel_recursive() {
+        assert_eq!(fuel_recursive(14), 2);
+        assert_eq!(fuel_recursive(1969), 966);
+        assert_eq!(fuel_recursive(100756), 50346);
+        assert_eq!(fuel_recursive(-1), 0);
+    }
 }
---
+++ src/bin/02.rs
@@ -0,0 +1,58 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input/02.in").unwrap();
+    let program: Vec<i64> = input
+        .lines()
+        .next()
+        .unwrap()
+        .split(",")
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let p1 = solve_p1(&program);
+    println!("Part One: {p1}");
+}
+
+fn solve_p1(program: &[i64]) -> i64 {
+    let mut state = program.to_owned();
+    state[1] = 12;
+    state[2] = 2;
+    run(&mut state)[0]
+}
+
+fn run(state: &mut [i64]) -> &[i64] {
+    let mut i = 0;
+    while state[i] != 99 {
+        let a = state[i + 1] as usize;
+        let b = state[i + 2] as usize;
+        let c = state[i + 3] as usize;
+        match state[i] {
+            1 => state[c] = state[a] + state[b],
+            2 => state[c] = state[a] * state[b],
+            _ => unreachable!(),
+        }
+        i += 4;
+    }
+    state
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run() {
+        assert_eq!(
+            run(&mut [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]),
+            [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
+        );
+        assert_eq!(run(&mut [1, 0, 0, 0, 99]), [2, 0, 0, 0, 99]);
+        assert_eq!(run(&mut [2, 3, 0, 3, 99]), [2, 3, 0, 6, 99]);
+        assert_eq!(run(&mut [2, 4, 4, 5, 99, 0]), [2, 4, 4, 5, 99, 9801]);
+        assert_eq!(
+            run(&mut [1, 1, 1, 4, 99, 5, 6, 0, 99]),
+            [30, 1, 1, 4, 2, 5, 6, 0, 99]
+        );
+    }
+}
