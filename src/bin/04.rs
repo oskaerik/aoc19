bfc9bbfda0a1d78a51737cdabd27d5d0351ac391
Oskar Eriksson <oskaerik@gmail.com>
2024-12-29 12:01:30 +0100

    Add 04 p1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -11,13 +11,20 @@ fn main() {
         .unwrap();
 
     let mut p1: i64 = 0;
+    let mut p2: i64 = 0;
     for x in a..b + 1 {
-        if password(x) {p1+=1};
+        if part1(x) {
+            p1 += 1
+        };
+        if part2(x) {
+            p2 += 1
+        };
     }
     println!("Part One: {p1}");
+    println!("Part Two: {p2}");
 }
 
-fn password(x: i64) -> bool {
+fn part1(x: i64) -> bool {
     let mut n = x;
     let mut d = 10;
     let mut inc = true;
@@ -33,14 +40,57 @@ fn password(x: i64) -> bool {
     inc && dub
 }
 
+fn part2(x: i64) -> bool {
+    let mut n = x;
+    let mut d = 10;
+    let mut c = 1;
+    let mut inc = true;
+    let mut dub = false;
+    while n > 0 {
+        let d_ = n % 10;
+
+        if d == d_ {
+            c += 1;
+        } else {
+            if c == 2 {
+                dub = true;
+            }
+            c = 1;
+        }
+
+        inc = inc && d_ <= d;
+
+        d = d_;
+        n /= 10;
+    }
+            if c == 2 {
+                dub = true;}
+    inc && dub
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_password() {
-        assert!(password(111111));
-        assert!(!password(223450));
-        assert!(!password(123789));
+    fn test_part1() {
+        assert!(part1(111111));
+        assert!(!part1(223450));
+        assert!(!part1(123789));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert!(part2(112233));
+        assert!(!part2(123444));
+        assert!(!part2(1234445));
+        assert!(part2(111122));
+        assert!(part2(1111223));
+        assert!(!part2(1111221));
+        assert!(!part2(123));
+        assert!(!part2(2111223));
+        assert!(!part2(211122));
+        assert!(!part2(0));
+        assert!(part2(00));
     }
 }
